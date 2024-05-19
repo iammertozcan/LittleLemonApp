98572f59111d46668b6826ecc82dc7ec0edb44ba
Mert Özcan <iammertozcan@gmail.com>
2024-05-20 01:01:09 +0300

    Add registration completion button

--- LittleLemon/LittleLemon/Onboarding.swift
+++ LittleLemon/LittleLemon/Onboarding.swift
@@ -5,6 +5,10 @@
 //  Created by Mert Özcan on 20.05.2024.
 //
 
+let kFirstName = "first_name_key"
+let kLastName = "last_name_key"
+let kEmail = "email_key"
+
 import SwiftUI
 
 struct Onboarding: View {
@@ -17,6 +21,13 @@ struct Onboarding: View {
             TextField("First Name", text: $firstName)
             TextField("Last Name", text: $lastName)
             TextField("Email", text: $email)
+            Button("Register") {
+                if !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty {
+                    UserDefaults.standard.set(firstName, forKey: kFirstName)
+                    UserDefaults.standard.set(lastName, forKey: kLastName)
+                    UserDefaults.standard.set(email, forKey: kEmail)
+                }
+            }
         }
     }
 }
