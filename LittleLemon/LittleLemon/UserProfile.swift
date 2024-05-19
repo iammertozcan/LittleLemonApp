d4a8d771a34786da26ab410554f9d1bc214183f4
Mert Özcan <iammertozcan@gmail.com>
2024-05-20 01:22:54 +0300

    Create new SwiftUI file for onboarding

---
+++ LittleLemon/LittleLemon/UserProfile.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfile.swift
+//  LittleLemon
+//
+//  Created by Mert Özcan on 20.05.2024.
+//
+
+import SwiftUI
+
+struct UserProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfile()
+}
