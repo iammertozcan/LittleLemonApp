90ef9ee8687660f4cb843b1a2154ecef1c77595e
Mert Özcan <iammertozcan@gmail.com>
2024-05-20 01:13:59 +0300

    Add Menu screen as tab item on Home screen

--- LittleLemon/LittleLemon/Home.swift
+++ LittleLemon/LittleLemon/Home.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct Home: View {
     var body: some View {
         TabView {
-            
+            Menu()
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
         }
     }
 }
