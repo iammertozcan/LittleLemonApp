79ff0c025116ec238bf09c0a07b5ee6ad78e044e
Mert Özcan <iammertozcan@gmail.com>
2024-05-20 01:11:37 +0300

    Add new Menu screen to hold menu information

---
+++ LittleLemon/LittleLemon/Menu.swift
@@ -0,0 +1,26 @@
+//
+//  Menu.swift
+//  LittleLemon
+//
+//  Created by Mert Özcan on 20.05.2024.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        VStack {
+            Text("Little Lemon")
+            Text("Chicago")
+            Text("We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.")
+            
+            List {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    Menu()
+}
