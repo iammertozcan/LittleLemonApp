2ef7c557d4d69aa5ef2aa9c4b6daecc365fcde4d
Mert Özcan <iammertozcan@gmail.com>
2024-05-20 01:05:47 +0300

    Add new home screen

---
+++ LittleLemon/LittleLemon/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  LittleLemon
+//
+//  Created by Mert Özcan on 20.05.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        TabView {
+            
+        }
+    }
+}
+
+#Preview {
+    Home()
+}
