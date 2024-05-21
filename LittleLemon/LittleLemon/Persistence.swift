403b46e6f97bad3086a04920025b12431a7358c9
Mert Özcan <iammertozcan@gmail.com>
2024-05-21 16:34:19 +0300

    Update Persistence file

---
+++ LittleLemon/LittleLemon/Persistence.swift
@@ -0,0 +1,28 @@
+import CoreData
+import Foundation
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    static var preview: PersistenceController = {
+        let controller = PersistenceController()
+        // Customize the controller as needed for previews
+        return controller
+    }()
+    
+    let container: NSPersistentContainer
+
+    init() {
+        container = NSPersistentContainer(name: "ExampleDatabase")
+        container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        container.loadPersistentStores(completionHandler: {_,_ in })
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    func clear() {
+        // Delete all dishes from the store
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "Dish")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        let _ = try? container.persistentStoreCoordinator.execute(deleteRequest, with: container.viewContext)
+    }
+}
