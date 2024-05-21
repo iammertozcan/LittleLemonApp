c80d8e372b36b25d7b82a91b7e3763be3c80f71c
Mert Özcan <iammertozcan@gmail.com>
2024-05-21 15:31:58 +0300

    Implement server querying

--- LittleLemon/LittleLemon/Menu.swift
+++ LittleLemon/LittleLemon/Menu.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct Menu: View {
+    @State private var menuItems: [MenuItem] = []
+    
     var body: some View {
         VStack {
             Text("Little Lemon")
@@ -18,7 +20,40 @@ struct Menu: View {
                 
             }
         }
+        .onAppear {
+            getMenuData()
+        }
     }
+    
+    func getMenuData() {
+        let urlString = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("Network error: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode(MenuList.self, from: data)
+                DispatchQueue.main.async {
+                    self.menuItems = decodedData.menu
+                }
+            } catch {
+                print("JSON Decoding error: \(error.localizedDescription)")
+            }
+        }
+        
+        task.resume()
+    }
+    
 }
 
 #Preview {
